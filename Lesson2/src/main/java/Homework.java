0ec8c29db4c7ca3cf0648d5461e0422c1934ad23
Asus <diima07081995@gmail.com>
2021-10-04 22:43:05 +0300

    домашка1

--- Lesson2/src/main/java/Homework.java
+++ Lesson2/src/main/java/Homework.java
@@ -3,20 +3,20 @@ import static java.lang.Integer.MAX_VALUE;
 public class Homework {
     public static void main(String[] args) {
         System.out.println(sum(100,200));
-       // System.out.println(sum(MAX_VALUE, MAX_VALUE));
+        System.out.println(sum(MAX_VALUE, MAX_VALUE));
         System.out.println(maximal(249, 60));
         System.out.println(average(new int[]{0, -2, 3, -1, 5}));
         System.out.println(max(new int[]{-1, -2, -3, -4, -40, -100, -99}));
         System.out.println(calculateHypotenuse(3,4));
-    }
+        }
+
 
     public static int sum (int a, int b) {
-        return (a+b);
-        long d = (long)a+b;
-            if (d > Integer.MAX_VALUE){
+
+            if ((long)a+b > Integer.MAX_VALUE ||(long)a+b < Integer.MIN_VALUE){
                 return (-1);
             }
-
+        return (a+b);
 
     }
     public static int maximal(int a, int b) {
@@ -47,5 +47,7 @@ public class Homework {
     }
        public static double calculateHypotenuse(int a, int b) {
            return Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+         //return  Math.hypot (a,b);
        }
+
     }
