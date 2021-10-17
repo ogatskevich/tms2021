d2c5a0c87b2c63497801fa7461191a2ec27467cd
Asus <diima07081995@gmail.com>
2021-10-17 22:23:34 +0300

    поправила замечания, не уверена, что методы верно работают

--- Lesson5/src/main/java/com/home/model/Computer.java
+++ Lesson5/src/main/java/com/home/model/Computer.java
@@ -2,81 +2,56 @@ package com.home.model;
 
 import java.util.Scanner;
 
-//class Program{
-//
-//    public static void main(String[] args) {
-//        Computer computer = new Computer();
-//        computer.description();
-//        computer.cpu = "Intell";
-//        computer.ram = 32;
-//        computer.hdd = 512;
-//        computer.resource=12;
-//        computer.description();
-//        computer.on();
-//    }
-//}
 public class Computer {
-    public String cpu;
-    public int ram;
-    public int hdd;
+    String cpu;
+    int ram;
+    int hdd;
     public int resource;
-
-    public Computer() {
-
-    }
+    int i;
 
     public void description() {
-        System.out.printf("CPU: %s \tRAM: %d\tHDD: %s \tCount: %s \t", cpu, ram, hdd, resource);
+        System.out.printf("CPU: %s \tRAM: %d\tHDD: %d \tCount: %d \t", cpu, ram, hdd, resource);
     }
 
-    public Computer(String cpu, int ram, int hdd, int resource) {
-        this.cpu = cpu;
-        this.ram = ram;
-        this.hdd = hdd;
-        this.resource = resource;
+    public Computer() {
+        cpu = "Intell";
+        ram = 32;
+        hdd = 512;
+        resource = 12;
     }
 
     public void on() {
-        for (int i = 0; i < resource; i++) {
-            if (i != resource) {
-                System.out.println("Внимание! Введите 0 или 1");
-                Scanner scanner = new Scanner(System.in);
-                int number = scanner.nextInt();
-                int random = (int) Math.random();
-                System.out.println(random);
-                if (number == random) {
-                    off();
-                    return;
-                } else {
-                    i = resource;
-                }
+        if (i != resource)
+            System.out.println("Внимание! Введите 0 или 1");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int random = (int) Math.random() * 2;
+        System.out.println(random);
+        if (number == random) {
+            off();
+        } else {
+            this.resource--;
+        }
+        System.out.println("Внимание! Компьютер сгорел");
+
+    }
+
+    public void off() {
+        if (resource > 0) {
+            System.out.println("Внимание! Введите 0 или 1");
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            int random = (int) Math.random();
+            System.out.println(random);
+            if (number == random) {
+                on();
             } else {
-                System.out.println("Внимание! Компьютер сгорел");
+                i = resource;
             }
+
+            System.out.println("Внимание! Компьютер сгорел");
         }
 
 
     }
-
-    private void off() {
-        for (int i = 0; i < resource; i++) {
-            if (i != resource) {
-                System.out.println("Внимание! Введите 0 или 1");
-                Scanner scanner = new Scanner(System.in);
-                int number = scanner.nextInt();
-                int random = (int) Math.random();
-                System.out.println(random);
-                if (number == random) {
-                    on();
-                    return;
-                } else {
-                    i = resource;
-                }
-            } else {
-                System.out.println("Внимание! Компьютер сгорел");
-            }
-        }
-    }
-
-
 }
--- Lesson5/src/main/java/com/home/service/Main.java
+++ Lesson5/src/main/java/com/home/service/Main.java
@@ -3,18 +3,15 @@ package com.home.service;
 import com.home.model.Computer;
 
 public class Main {
+    private static Object resource;
+
     public static void main(String[] args) {
         // com.home.model.Computer computer = new com.home.model.Computer();
         Computer computer = new Computer();
         computer.description();
-        computer.cpu = "Intell";
-        computer.ram = 32;
-        computer.hdd = 512;
-        computer.resource = 12;
-        computer.description();
-        computer.on();
-
+        for (int i = 0; i != computer.resource; i++) {
+            computer.on();
+            computer.off();
+        }
     }
-
-
 }
